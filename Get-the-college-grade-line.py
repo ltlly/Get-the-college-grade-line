ef9a80c30f4c179862e8778280bc938ae9acbe1c
洛天琉璃依 <86669384+ltlly@users.noreply.github.com>
2021-07-06 12:43:04 +0800

    Update Get-the-college-grade-line.py

--- Get-the-college-grade-line.py
+++ Get-the-college-grade-line.py
@@ -30,43 +30,44 @@ def get_zhuanye(i):
         return ["计算机", "无"]
     elif not computer and not software:
         return ["无", "无"]
+
+#检测985 211
 def is_985_211(i):
     resp_is = requests.get(url=f'https://static-data.eol.cn/www/2.0/school/{i}/info.json',
-                                headers={'User-Agent': 'BaiduSpider', })
+                           headers={'User-Agent': 'BaiduSpider', })
     resp_is.encoding = "utf-8"
-    pattern_985=re.compile(r'"f985":"1"')
-    is_985=pattern_985.findall(resp_is.text)
-    pattern_211=re.compile(r'"f211":"1"')
-    is_211=pattern_211.findall(resp_is.text)
-    if is_985:
+    resp_is = resp_is.json()
+    resp_is = resp_is["data"]
+    if resp_is["f985"] == "1":
         return "985"
-    elif is_211:
+    elif resp_is["f211"] == "1":
         return "211"
     else:
         return ""
+
+
 # 获取大学名称
 def get_name(resp):
     # resp:request获取的网址
     # 正则表达式获取包含大学名称的字符串
-    name = re.compile(r"\"name\":\".{1,500}\",\"type\"")
-    results_name_ucode = name.findall(resp.text)
-    # 通过编码转换方法的到中文学校名称 并移除多余部分
-    results_name=""
-    if results_name_ucode:
-        results_name = filter_u_code(results_name_ucode[0]).replace("\"name\":\"", "").replace("\",\"type\"", "")
+    resp_data = resp.json()
+    name = resp_data["data"]["name"]
+    results_name = filter_u_code(name)
     return results_name
 
 
 # 获取大学成绩
 def get_score(resp):
     # 通过正则表达式获取分数线的字符串
-    scores_many_years = re.compile(
-        r'"13":\[{"year":2020,"type":{.{0,500}"}}],"14":\[{"year":2020,"type":{')
-    results_score = scores_many_years.findall(resp.text)
-    # 遍历并提取所有数字~
-    for index in range(len(results_score)):
-        results_score[index] = re.findall("\d\d\d", str(results_score[index]))
-    return results_score
+    resp_data = resp.json()
+    results_score = []
+    try:
+        for index in range(0, len(resp_data["data"]["pro_type_min"]["13"])):
+            scores = resp_data["data"]["pro_type_min"]["13"][index]["type"]["1"]
+            results_score.append(int(float(scores)))
+        return results_score
+    except (KeyError):
+        pass
 
 
 # 打印excel
@@ -76,7 +77,7 @@ def output(scores_dic, output_file_name):
     name_score = xlwt.Workbook()
     print(scores_dic)
     sheet = name_score.add_sheet('分数线')
-    titles = ("名称", "2020分数线平均", "2020分数线最低", "2019分数线平均", "2019分数线最低", "2018分数线平均", "2018分数线最低", "1号专业", "2号专业")
+    titles = ("名称", "2020分数线平均", "2019分数线平均", "2018分数线平均", "1号专业", "2号专业")
     row = 1
     # 创建标题
     for index, title in enumerate(titles):
@@ -87,8 +88,8 @@ def output(scores_dic, output_file_name):
             # 书写学校名称
             sheet.write(row, 0, key)
             # 依次打印
-            sheet.write(row, 7, scores_dic[key][-2])
-            sheet.write(row, 8, scores_dic[key][-1])
+            sheet.write(row, 4, scores_dic[key][-2])
+            sheet.write(row, 5, scores_dic[key][-1])
             for line in range(len(scores_dic[key]) - 2):
                 sheet.write(row, line + 1, scores_dic[key][line])
             row += 1
@@ -97,29 +98,23 @@ def output(scores_dic, output_file_name):
 
 def main():
     scores_dic = {}
-    #0,3602
-    #3329
-    for i in range(0, 3602):
+    # 30,3602
+    # 3329
+    for i in range(30, 51):
 
         resp = requests.get(url=f'https://static-data.eol.cn/www/2.0/school/{i}/info.json',
                             headers={'User-Agent': 'BaiduSpider'})
-        # 通过数数的方式判断是不是一个空的网址id(空的网址counter为2
-        counter = 0
-        for index in resp.text:
-            counter += 1
-        if counter == 2:
+        resp_json=resp.json()
+        # 判断是不是一个空的网址id
+        try:
+            resp_json["data"]
+        except(KeyError):
             continue
         # 获取名称~
-        results_name = get_name(resp)+is_985_211(i)
+        results_name = get_name(resp) + is_985_211(i)
         results_score = get_score(resp)
         # 排除技校和没有分的大学
         if results_score and not "职业" in results_name:
-            results_score = results_score[0]
-            # 仅保留分数
-            while "202" in results_score:
-                results_score.remove("202")
-            while "201" in results_score:
-                results_score.remove("201")
             # 获取专业情况并拼接列表
             results_score = results_score + get_zhuanye(i)
             # 检测是否具有计算机和软件工程专业
@@ -129,11 +124,11 @@ def main():
                 scores_dic.update(new_name_score)
                 # 打印一下进度条
                 print(new_name_score)
-                print(i, "号大学       ", round(i * 100 / 3680,2), "%")
+                print(i, "号大学       ", round(i * 100 / 3680, 2), "%")
     return scores_dic
 
 
 if __name__ == '__main__':
     scores_dic = main()
-    numpy.save('分数线字典(包含专业.npy', scores_dic)
-    output(scores_dic, "分数线_new")
+    # numpy.save('分数线字典(包含专业.npy', scores_dic)
+    output(scores_dic, "测试1.xls")
